113de0fa75cfd98884f2a8a5fcfda9f67ce0227e
Al Albers <albert.albers@identifix.com>
2020-11-30 13:32:11 -0600

    aa

--- src/Extras/Write-Splash.ps1
+++ src/Extras/Write-Splash.ps1
@@ -2,17 +2,31 @@
 #-----------------------------------------------------------------------
 #-----------------------------------------------------------------------
 function Write-Splash([string] $msg, [switch] $invert) {
-    
     if ($invert.IsPresent) {
         Write-HR;
     }
     Write-Host "== " -ForegroundColor DarkBlue -NoNewline;
     Write-Host $msg -ForegroundColor Cyan -NoNewline;
     Write-Host " ==" -ForegroundColor DarkBlue;
-    
+
     if (!$invert.IsPresent) {
         Write-HR;
     }
+}
+#-----------------------------------------------------------------------
+#-----------------------------------------------------------------------
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+#-----------------------------------------------------------------------
+#-----------------------------------------------------------------------
+function Write-InvokeBlock([ScriptBlock] $sb, [Object[]] $argList) {
+    Write-HR-Half;
+    Write-Host $sb.ToString().Replace("        ", "") -ForegroundColor Green;
+    Write-Host $argList -ForegroundColor Yellow;
+    Write-HR-Half;
+    Write-NewLine;
+    Invoke-Command -ScriptBlock $sb -ArgumentList $argList;
 }
 #-----------------------------------------------------------------------
 #-----------------------------------------------------------------------
