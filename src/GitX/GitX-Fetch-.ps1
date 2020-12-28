5e772ecd91e682d32420e81e597b0758fa37e2c3
Al Albers <albert.albers@identifix.com>
2020-12-28 10:21:38 -0600

    add switch: prune_local

--- src/GitX/GitX-Fetch-.ps1
+++ src/GitX/GitX-Fetch-.ps1
@@ -2,13 +2,19 @@
 #-----------------------------------------------------------------------
 #-----------------------------------------------------------------------
 function GitX-Fetch {
+    [CmdletBinding()]
+    Param (
+        [switch] $prune_local
+    )
     git fetch --all --prune --tags;
     $regex = [System.Text.RegularExpressions.Regex]::new("^(.*?)\[origin/(.*?): gone\](.*?)$", [System.Text.RegularExpressions.RegexOptions]::Multiline);
-    git branch -ravv | ForEach-Object { if ($regex.IsMatch($_)) {
-            git branch -D $($regex.Match($_).Groups[2].Value);
-        }
-    };
-    GBA;
+    if ($prune_local.IsPresent) {
+        git branch -ravv | ForEach-Object { if ($regex.IsMatch($_)) {
+                git branch -D $($regex.Match($_).Groups[2].Value);
+            }
+        };
+    }
+    GitX-Branch-All;
 }
 #-----------------------------------------------------------------------
 #-----------------------------------------------------------------------
