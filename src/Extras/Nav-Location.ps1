a143e6f9e5618e17dd5dc16cee156d59d1977a9a
Al Albers <albert.albers@identifix.com>
2021-01-20 11:21:07 -0600

    navlocs

--- src/Extras/Nav-Location.ps1
+++ src/Extras/Nav-Location.ps1
@@ -16,14 +16,16 @@ function Nav-Location {
         Set-Location $location;
         return;
     }
+
     $pindices = [System.Collections.Generic.List[string]]::new();
     $places = [System.Collections.Generic.Dictionary[string, string]]::new();
-    Get-ChildItem -Path "~"  -Directory | ForEach-Object {
+    Get-ChildItem -Path "~" -Directory | ForEach-Object {
         if ([System.IO.Directory]::Exists($_.FullName + "\.git")) {
             $pindices.Add($_.Name);
             $places.Add($_.Name, $_.FullName);
         }
     }
+
     $option = (Console-Menu "places" ("git repositories", "vscode workspaces", "vs solutions"));
 
     switch ($option) {
@@ -34,18 +36,35 @@ function Nav-Location {
             break;
         }
         1 {
-            $windices = [System.Collections.Generic.List[string]]::new();
-            $workspaces = [System.Collections.Generic.Dictionary[string, string]]::new();
-            $places.Values | ForEach-Object {
-                Get-ChildItem -Path $_ -File -Filter "*.code-workspace" -Recurse | ForEach-Object {
-
-                    $windices.Add($_.Name);
-                    $workspaces.Add($_.Name, $_.FullName);
-                };
-            };
-            $windex = (Console-Menu "vscode workspaces" $windices.ToArray());
-            code $workspaces[$windices[$windex]];
+            code (menuList $places "*.code-workspace" "vscode workspaces" 3);
+            break;
+        }
+        2 {
+            devenv (menuList $places "*.sln" "vs solutions" 3);
+            break;
         }
         default { break; }
     }
+}
+
+function menuList([System.Collections.Generic.Dictionary[string, string]] $places, [string] $filter, [string] $title,[uint] $depth) {
+
+    $windices = [System.Collections.Generic.List[string]]::new();
+    $workspaces = [System.Collections.Generic.Dictionary[string, string]]::new();
+    $places.Values | ForEach-Object {
+        Get-ChildItem -Path $_ -File -Filter $filter -Recurse -Depth $depth | ForEach-Object {
+            $key = $_.Name;
+            while ($workspaces.ContainsKey($key)) {
+                $key += "_";
+            }
+            $windices.Add($key);
+            $workspaces.Add($key, $_.FullName);
+        };
+    };
+    $windex = (Console-Menu $title $windices.ToArray());
+    $workspace = $workspaces[$windices[$windex]];
+    $directory = ([System.IO.FileInfo]::new($workspace)).DirectoryName;
+    Push-Location;
+    Set-Location (GitX-Util-Repo-Root $directory);
+    return $workspace;
 }
