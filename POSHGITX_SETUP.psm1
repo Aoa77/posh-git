ee7621786de2a6b4bffd56924cecf6977bce6dcc
aoa77 <alalbers77@gmail.com>
2021-06-25 14:24:21 -0500

    aa

--- POSHGITX_SETUP.psm1
+++ POSHGITX_SETUP.psm1
@@ -2,33 +2,54 @@
 [string] $POSHGITX_folder = "";
 [string] $POSHGITX_file = "";
 
+function LOG_INFO ([string] $msg) {
+    Write-Information "INFO: $msg";
+}
+function LOG_DEBUG ([string] $msg) {
+    Write-Debug "$msg";
+}
+function LOG_VERBOSE ([string] $msg) {
+    Write-Verbose "$msg";
+}
+function LOG_WARNING ([string] $msg) {
+    Write-Warning "$msg";
+}
+function LOG_ERROR ([string] $msg) {
+    Write-Error "$msg";
+}
+
 function POSHGITX_ACTION {
     Write-Host;
     Write-Host "== " -ForegroundColor DarkBlue -NoNewline;
     Write-Host $POSHGITX_action -ForegroundColor Cyan -NoNewline;
     Write-Host " ==" -ForegroundColor DarkBlue;
 }
+function POSHGITX_BOOKEND {
+    Write-Host;
+    Write-Host "== " -ForegroundColor DarkGreen -NoNewline;
+    Write-Host $POSHGITX_action -ForegroundColor Green -NoNewline;
+    Write-Host " ==" -ForegroundColor DarkGreen;
+}
 function POSHGITX_CONCAT {
     $concat = [string]::Concat($PSScriptRoot, $POSHGITX_folder, $POSHGITX_file);
-    Write-Debug "POSHGITX_CONCAT: $concat";
+    LOG_DEBUG "POSHGITX_CONCAT: $concat";
     return $concat;
 }
 function POSHGITX_IMPORT {
-    Import-Module -Force -Verbose -Scope "Global" -Name "$(POSHGITX_CONCAT)";
+    Import-Module -Force -Scope "Global" -Name "$(POSHGITX_CONCAT)";
 }
-function POSHGITX_CHILDREN([bool] $logSideways) {
-    Write-Debug "POSHGITX_CHILDREN -logSideways: $logSideways";
+function POSHGITX_CHILDREN([) {
     $children = (Get-ChildItem -Path "$(POSHGITX_CONCAT)" -Filter "*.psm1");
     $children | ForEach-Object {
-        Write-Debug $_;
-        Import-Module -Force -Verbose -Scope "Global" -Name "$_";
-        Write-Verbose "";
+        LOG_DEBUG $_;
+        Import-Module -Force -Scope "Global" -Name "$_";
+        LOG_DEBUG;
     };
 }
 
 function POSHGITX_SETUP {
     $POSHGITX_action = "posh-gitX setup script";
-    POSHGITX_ACTION;
+    POSHGITX_BOOKEND;
 
     $POSHGITX_action = "resetting alias definitions";
     $POSHGITX_folder = "\";
@@ -47,40 +68,30 @@ function POSHGITX_SETUP {
     $POSHGITX_folder = "\srcX\WriteX\";
     $POSHGITX_file = "";
     POSHGITX_ACTION;
-    POSHGITX_CHILDREN $false;
+    POSHGITX_CHILDREN;
 
     $POSHGITX_action = "importing GitX modules";
     $POSHGITX_folder = "\srcX\GitX\";
     $POSHGITX_file = "";
-    WriteX_SidewaysBufferReset;
-    WriteX_NewLine 1;
     POSHGITX_ACTION;
-    POSHGITX_CHILDREN $false;
+    POSHGITX_CHILDREN;
 
     $POSHGITX_action = "importing ConsoleX modules";
     $POSHGITX_folder = "\srcX\ConsoleX\";
     $POSHGITX_file = "";
-    WriteX_SidewaysBufferReset;
-    WriteX_NewLine 2;
     POSHGITX_ACTION;
-    POSHGITX_CHILDREN $false;
+    POSHGITX_CHILDREN;
 
     $POSHGITX_action = "importing HotkeyX modules";
     $POSHGITX_folder = "\srcX\HotkeyX\";
     $POSHGITX_file = "";
-    WriteX_SidewaysBufferReset;
-    WriteX_NewLine 2;
     POSHGITX_ACTION;
-    POSHGITX_CHILDREN $false;
+    POSHGITX_CHILDREN;
 
     $POSHGITX_action = "creating alias definitions";
     POSHGITX_ACTION;
-    POSHGITX_ALIASES $false;
+    POSHGITX_ALIASES;
 
     $POSHGITX_action = "posh-gitX setup complete :)";
-    WriteX_SidewaysBufferReset;
-    WriteX_NewLine 1;
-    Start-Sleep -Milliseconds 1;
-    WriteX_NewLine 2;
-    WriteX_Splash $POSHGITX_action;
+    POSHGITX_BOOKEND;
 }
